4d31de703129fcfe8675130b330c24856ec31333
= <huey.j@kakaocorp.com>
2021-02-04 00:22:47 +0900

    sql : data 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO BOX (id, name) VALUES (1, 'box')
 INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (1, 1, 0, 'text', '이름', '준호')
+INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (2, 1, 1, 'textArea', '소개', '준호\nHi')
