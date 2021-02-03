05c263bdf3fa369a56d909bd25ec3fceb4febf24
= <huey.j@kakaocorp.com>
2021-02-04 00:34:50 +0900

    feat : item seq에 따른 정렬

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO BOX (id, name) VALUES (1, 'box')
-INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (1, 1, 0, 'text', '이름', '준호')
-INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (2, 1, 1, 'textArea', '소개', '준호\nHi')
+INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (1, 1, 1, 'text', '이름', '준호')
+INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (2, 1, 0, 'textArea', '소개', '준호\nHi')
