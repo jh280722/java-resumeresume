50acd607f70131821cae7fb0544f41f69f0581e0
Ryu hanmin <jin74137@gmail.com>
2021-02-16 20:29:05 +0900

    document crud 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO ITEM (id, box_id, seq, type, name, value) VALUES (2, 1, 0, 'textArea', '소개', '준호\nHi')
 InSERT INTO DOCUMENT (id,name) VALUES(1,'document')
 INSERT INTO BOX (id, name, document_id) VALUES (1, 'box',1)
